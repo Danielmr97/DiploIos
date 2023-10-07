ce58eeaa3d1d1435ac9d169d21a505b5059b03bb
Daniel Mayo <jesusdmr97@gmail.com>
2023-10-06 21:08:15 -0600

    Ejercicio extra

--- 1App22deSep/1App22deSep/TwitModallViewController.swift
+++ 1App22deSep/1App22deSep/TwitModallViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class TwitModallViewController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        MessageTexView.text = ""
+    }
 
-    @IBOutlet weak var MessageTexView: UITextView!
+    @IBOutlet weak var MessageTexView: UITextView! {
+        didSet{
+            MessageTexView.delegate = self
+        }
+    }
+    
     @IBOutlet weak var SendingButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
+    @IBAction func SendInfoButton(_ sender: UIButton) {
+        print(MessageTexView.text)
+    }
+}
+
+extension TwitModallViewController : UITextViewDelegate {
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        if text == ""{return true }
+        if text.rangeOfCharacter(from: .alphanumerics) == nil && text != " "{
+            return false
+        } else {
+            return ((textView.text?.count ?? 0) + text.count - range.length <= 150)
+        }
+    }
 }
